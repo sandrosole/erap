6b8348c87c3c6425da89ee192014bb5cc33555a6
sandrosole <128021802+sandrosole@users.noreply.github.com>
2024-06-14 23:15:21 +0700

    nilai

---
+++ application/views/nilai/dataview.php
@@ -0,0 +1,23 @@
+<h3>Judul halaman</h3>
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+            <h5>Tabel Mapel</h5>
+        </div>
+        <div class="card-body">
+            <table class="table" id="basic-datatables">
+                <thead>
+                <tr>
+                      <th>No</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    
+    </div>
+</div>
